25cff2b0a0902dbcf22fcff25798342585c2a4e9
Franco Caraballo <franconcaraballo@gmail.com>
2024-06-11 17:47:31 -0300

    Primera pre entrega

---
+++ script.js
@@ -0,0 +1,81 @@
+let descripcionCompra = ''
+let totalCompra = 0;
+let seguirComprando = true;
+
+do {
+    let producto = 0;
+    let cantidadProductos = 0;
+
+    // Validacion de los campos de entrada
+
+    while (!producto || !cantidadProductos) {
+        producto = parseInt(prompt(`Que producto desea comprar? (Ingrese solo el numero)
+        1 - Fernet Branca 750 ml - $9.000
+        2 - Gancia 1 L - $5.000
+        3 - Gin Gordons 700 ml $8.500
+        4 - Vodka Absolut Raspberri 700 ml - $18.000
+        5 - Jägermeister Jagger 700 ml - $24.000
+        `));
+
+        cantidadProductos = parseInt(prompt("Cuantas unidades quiere agregar?"));
+
+        if(producto > 5 || producto < 1 || cantidadProductos < 0) {
+            producto = 0;
+            cantidadProductos = 0;
+            alert('Por favor, ingrese un numero valido!')
+        }
+
+    }
+
+    // Listado de productos, donde se asignas los precios y nombres correspondientes
+
+    let productoNombre = '';
+    let precioUnitario = 0;
+
+    switch (producto) {
+        case 1:
+            precioUnitario = 9000;
+            productoNombre = 'Fernet Branca 750 ml'
+            break;
+
+        case 2:
+            precioUnitario = 5000;
+            productoNombre = 'Gancia 1 L'
+            break;
+
+        case 3:
+            precioUnitario = 8500;
+            productoNombre = 'Gin Gordons 700 ml'
+            break;
+
+        case 4:
+            precioUnitario = 18000;
+            productoNombre = 'Vodka Absolut Raspberri 700 ml'
+            break;
+
+        case 5:
+            precioUnitario = 24000;
+            productoNombre = 'Jägermeister Jagger 700 ml'
+            break;
+
+        default:
+            alert("Por favor, ingrese un numero valido!")
+            break;
+    }
+
+    let precioTotal = cantidadProductos * precioUnitario;
+
+    descripcionCompra += `${cantidadProductos}u - ${productoNombre} - $${precioTotal}\n`;
+
+    totalCompra += precioTotal;
+
+    seguirComprando = confirm("Quiere seguir comprando?")
+} while (seguirComprando)
+
+// Se muestra el detalle de la compra realizada
+descripcionCompra += `\nTOTAL: $${totalCompra}`
+alert(descripcionCompra);
+
+
+
+
